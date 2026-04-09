7823ca4a3c70c7738f540a7877ca87b939c799b0
Christopher Preston <cjpcjpcjp1973@gmail.com>
2026-04-08 21:49:24 -0400

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,23 +1,6 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-dom";
-import path from "dist";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
-export default defineConfig(({ mode }) => ({
-  server: {
-    host: "https://www.fishkillerz.com",
-    port: 8080,
-    hmr: {
-      overlay: false,
-    },
-  },
-  plugins: [
-    react(),
-    mode === "production"(),
-  ].filter(Boolean),
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
-  },
-}));
+export default defineConfig({
+  plugins: [react()]
+})
